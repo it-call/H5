fe279f7d5632ba3d4eea29a12abe0f9c41cc483e
it_call <1486522507@qq.com>
2019-01-09 19:20:26 +0800

    VueProject

---
+++ VueJS/VuePrj/yikang/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <ul>
+            <li><router-link to="/home" tag="strong" active-class="current">Home</router-link></li>
+            <li><router-link to="/cart" active-class="current">Cart</router-link></li>
+            <li><router-link to="/mine" active-class="current">Mine</router-link></li>
+        </ul>
+        <router-view/>
+    </div>
+</template>
+
+<script>
+export default {
+//   name: 'App'
+}
+</script>
+
+<style>
+    .current{color: red;font-weight: bold;}
+</style>
---
+++ VueJS/VuePrj/yikang/src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+// 引入vue-router
+import VueRouter from 'vue-router';
+// 使用插件
+Vue.user(VueRouter); 
+
+// 引入路由组件
+import Home from '@com/Home';
+import Cart from '@com/Cart';
+import Mine from '@com/Mine';
+
+// 组件
+const Home={
+    template:`<div>首页</div>`
+},
+const Cart={
+    template:`<div>购物车</div>`
+},
+const Mine={
+    template:`<div>我的</div>`
+},
+
+// 实例化路由
+const routes=[
+    // {path:'/',component:Home},
+    {name:'Home',path:'/home',component:Home},
+    {name:'Cart',path:'/cart',component:Cart},
+    {name:'Mine',path:'/mine',component:Mine},
+    {path:'/',redirect:{name:'Home'}}/* 重定向 */
+]
+let router=new VueRouter({
+    routes
+});
+
+export default router;
